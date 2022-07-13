60a666a694f969df989486eb786f14f935dde31b
TheOpenCompanyo <99085626+TheOpenCompanyo@users.noreply.github.com>
2022-07-13 20:34:12 +0530

    Delete Telegram Bot/telegram bot directory

--- Telegram Bot/telegram bot/serverModule.py
+++
@@ -1,5 +0,0 @@
-from telethon.sync import TelegramClient
-from telethon.tl.types import InputPeerUser, InputPeerChannel
-from telethon import TelegramClient, sync, events
-import asyncio
-import threading
